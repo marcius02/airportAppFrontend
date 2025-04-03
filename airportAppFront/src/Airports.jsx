0f4093693e4c66dff4da5261309d80b14ab06f5c
marcius02 <marcfernandezz1011@gmail.com>
2025-04-03 11:53:31 +0200

    feat: retrieve airports

--- airportAppFront/src/Airports.jsx
+++ airportAppFront/src/Airports.jsx
@@ -1,11 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "./api";
+
+const Airports = () => {
+    const [airports, setAirports] = useState([]);
+
+    const fetchAirports = async () => {
+        try{
+            const response = await axios.get("http://localhost:8080/api/airports");
+            setAirports(response.data);
+        } catch (error) {
+            console.error("Error fetching airports:", error);
+        }
+    };
 
 
+    // Fetch airports on component mount
+    useEffect(() => {
+        fetchAirports();
+    }, []);
 
-
-export default function Airports() {
-    return(
+    return (
         <>
-            <h1>Airports</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Code</th>
+                        <th>City</th>
+                        <th>Country</th>
+                    </tr>
+                </tbody>
+                <tbody>
+                    {airports.map((airport) => (
+                        <tr key={airport.id}>
+                            <td>{airport.name}</td>
+                            <td>{airport.code}</td>
+                            <td>{airport.city}</td>
+                            <td>{airport.country}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
-    );   
+    )
 }
+
+
+
+export default Airports;
