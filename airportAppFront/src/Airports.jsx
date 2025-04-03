8aacf45ca0929e289919c484009681613bce0e45
marcius02 <marcfernandezz1011@gmail.com>
2025-04-03 12:38:20 +0200

    feat: delete airport

--- airportAppFront/src/Airports.jsx
+++ airportAppFront/src/Airports.jsx
@@ -4,6 +4,7 @@ import axios from "./api";
 const Airports = () => {
     const [airports, setAirports] = useState([]);
 
+    // Fetch all airports
     const fetchAirports = async () => {
         try{
             const response = await axios.get("http://localhost:8080/api/airports");
@@ -13,6 +14,16 @@ const Airports = () => {
         }
     };
 
+    // Delete an airport by ID
+    const deleteAirport = async (id) => {
+        try{
+            await axios.delete(`http://localhost:8080/api/airports/${id}`);
+            // Update the state to remove the deleted book
+            setAirports(airports.filter((airport) => airport.id !== id));
+        } catch (error){
+            console.error("Error deliting airport:", error);
+        }
+    }
 
     // Fetch airports on component mount
     useEffect(() => {
@@ -37,6 +48,13 @@ const Airports = () => {
                             <td>{airport.code}</td>
                             <td>{airport.city}</td>
                             <td>{airport.country}</td>
+                            <td>
+                                <button
+                                    onClick={() => deleteAirport(airport.id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
